66c65f21f61857276c8091ea416f20d6684c7196
Amr Tawela <32248367+Amr-Tawela@users.noreply.github.com>
2022-07-09 22:30:23 +0200

    Update covidcase.sql

--- covidcase.sql
+++ covidcase.sql
@@ -148,11 +148,12 @@ SELECT SUM(new_cases::INTEGER) new_cases, SUM(new_deaths::INTEGER) new_deaths,
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths,
        SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-  From PortfolioProject..CovidDeaths
+  From coviddeaths
 --Where location like '%states%'
  where continent is not null 
 --Group By date
  order by 1,2
+
 
 
 -- Just a double check based off the data provided
@@ -173,28 +174,29 @@ Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deat
 -- European Union is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
+  From coviddeaths
 --Where location like '%states%'
-Where continent is null 
-and location not in ('World', 'European Union', 'International')
-Group by location
-order by TotalDeathCount desc
+ Where continent is null 
+       and location not in ('World', 'European Union', 'International')
+ Group by location
+ order by TotalDeathCount desc
 
 
 -- 3.
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+  From coviddeaths
 --Where location like '%states%'
-Group by Location, Population
-order by PercentPopulationInfected desc
+ Group by Location, Population
+ order by PercentPopulationInfected desc
 
 
 -- 4.
 
 
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  
+       Max((total_cases/population))*100 as PercentPopulationInfected
+  From coviddeaths
 --Where location like '%states%'
-Group by Location, Population, date
-order by PercentPopulationInfected desc
+ Group by Location, Population, date
+ order by PercentPopulationInfected desc
