12c2f453b092838e8d21969a36973938030d921d
Amr Tawela <32248367+Amr-Tawela@users.noreply.github.com>
2022-07-26 11:27:55 +0200

    Update covidcase.sql

--- covidcase.sql
+++ covidcase.sql
@@ -1,202 +1,135 @@
+USE covid_19
 SELECT * 
-  FROM coviddeaths 
+  FROM covid_deaths
+ ORDER BY 3,4 ;
 
 SELECT * 
-  FROM covidvaccinations 
+  FROM covid_vaccinations
+ ORDER BY 3,4;
 
--- (select data that we are going to be using)
-SELECT location , date , total_cases , new_cases , total_deaths, population 
-  FROM coviddeaths
- ORDER BY  1,2
- 
--- (looking at total cases vs total deaths) 
-SELECT location , date , total_cases , total_deaths , 
-       (total_deaths::REAL / total_cases::REAL)* 100  death_percentage ,
-  FROM coviddeaths 
- WHERE location LIKE '%States%'
- ORDER BY 1,2
+--SELECT data that I am going to be using 
+SELECT location, date, total_cases , new_cases , total_deaths , population 
+  FROM covid_deaths 
+ ORDER BY 1,2 
 
--- (looking at total cases vs population )
-SELECT location , date , total_cases , population , 
-       (total_cases::REAL / population::REAL)* 100  cases_percentage 
-  FROM coviddeaths 
- WHERE location LIKE '%States%'
- ORDER BY 1,2
+ --looking at total cases vs total deaths (death_percentage)
+ --shows liklihood of dying if you are infected covid in Egypt
+CREATE VIEW egypt_death_percentage AS 
+SELECT location, date, total_cases , total_deaths , ROUND((total_deaths/total_cases),4)*100 AS death_percentage
+  FROM covid_deaths 
+ WHERE location = 'Egypt'
+-- ORDER BY 1,2 DESC
 
--- (looking at countries with highest infection rate compared to population )
-WITH cte AS 
-(
-SELECT location , date , total_cases , population , 
-       (total_cases::REAL / population::REAL)* 100  cases_percentage 
-  FROM coviddeaths 
- ORDER BY 1,2
-)
+ --showing total cases vs population 
+ --shows what percentage of Egypt's population infected by covid 
+CREATE VIEW egypt_infection_percentage AS
+SELECT location, date, population , total_cases , ROUND((total_cases/population),4)*100 AS infected_percentage
+  FROM covid_deaths 
+ WHERE location = 'Egypt'
+ --ORDER BY 1,2 
 
-SELECT location ,MAX(cases_percentage) 
-  FROM cte
- GROUP BY location 
- ORDER BY 2 DESC NULLS LAST
- 
--- (showing countries with highest death count per population) 
-WITH cte2 AS 
-(
-SELECT location , continent , date , total_deaths::INT , population , 
-       (total_deaths::REAL / population::REAL)* 100  death_percentage 
-  FROM coviddeaths 
-  WHERE continent IS NOT NULL
- ORDER BY 1,2
-)
+--Showing new_cases,new_deaths per day in egypt , death_percentage per day 
+CREATE VIEW egypt_cases_deaths_per_day AS
+SELECT date, SUM(new_cases) new_cases , SUM(CAST(new_deaths AS INT)) new_deaths , 
+       ROUND(SUM(CAST(new_deaths AS INT))/NULLIF(SUM(new_cases),0)*100,2) death_percentage_per_day
+  FROM covid_deaths
+ WHERE location = 'Egypt'
+ GROUP BY date 
+ --ORDER BY 1 
 
-SELECT location , SUM(total_deaths) total_deaths_per_country
-  FROM cte2
+--Showing total_cases , total_deaths in EGYPT
+CREATE VIEW egypt_total_cases_deaths AS
+SELECT SUM(new_cases) total_cases , SUM(CAST(new_deaths AS INT)) total_deaths , 
+       ROUND(SUM(CAST(new_deaths AS INT))/NULLIF(SUM(new_cases),0)*100,2) death_percentage
+  FROM covid_deaths
+ WHERE location = 'Egypt'
+ --ORDER BY 1 
+
+ --Comparing Egypt to world and africa 
+
+--showing countries with highest infection rate compared to population
+CREATE VIEW world_highest_infection_rate_compared_population AS
+SELECT location, population ,MAX(total_cases) highest_infection_per_country,
+         ROUND((MAX(total_cases)/population),4)*100 AS infected_percentage,
+		 RANK() OVER (ORDER BY ROUND((MAX(total_cases)/population),4) DESC) highest_infection_rate_compared_to_population_rank
+  FROM covid_deaths 
  WHERE continent IS NOT NULL
- GROUP BY 1
- ORDER BY 2 DESC NULLS LAST
-
--- (Drill up by continent)
-WITH cte AS 
-(
-SELECT location, continent , date , total_cases , population , 
-       (total_cases::REAL / population::REAL)* 100  cases_percentage 
-  FROM coviddeaths 
- WHERE continent IS  NULL
- ORDER BY 1,2
-)
-
-SELECT location  , MAX(total_cases)
-  FROM cte 
- WHERE continent IS NULL
+ GROUP BY location , population 
+ --ORDER BY 4 DESC 
+ 
+ --showing countries with highest death count per population 
+CREATE VIEW world_highest_death_rate_compared_population AS
+SELECT location , MAX(CAST(total_deaths AS INT)) AS total_death_count
+  FROM covid_deaths 
+ WHERE continent IS NOT NULL
  GROUP BY location 
- ORDER BY 2 DESC
+ --ORDER BY 2 DESC 
 
--- (showing continents with the highest death count per population) 
-SELECT continent , MAX(total_deaths::integer) 
-  FROM coviddeaths
- WHERE continent IS NOT NULL AND (location NOT IN ('World','International'))
- GROUP BY continent 
- ORDER BY 2 DESC;
+--showing countries with highest death count per total_cases 
+CREATE VIEW  world_highest_death_per_total_cases AS
+SELECT location ,MAX(total_deaths) max_total_deaths,
+       MAX(total_cases) max_total_cases, (MAX(total_deaths)/MAX(total_cases))*100 highest_death_per_infections
+  FROM covid_deaths 
+ WHERE continent IS NOT NULL AND location != 'North Korea'
+ GROUP BY location 
+ --ORDER BY 4 DESC
 
--- (global numbers)
-SELECT SUM(new_cases::INTEGER) new_cases, SUM(new_deaths::INTEGER) new_deaths,
-       (SUM(new_deaths::REAL)/SUM(new_cases::REAL))*100 death_percentage
-  FROM coviddeaths 
- WHERE continent IS NOT NULL 
- ORDER BY 1,2,3 NULLS LAST
+--Looking at total population vs total vaccinations 
+CREATE VIEW egypt_total_population_vs_total_vaccinations AS
+SELECT de.location , de.date , de.population , CAST(va.new_vaccinations AS INT) new_vaccinations ,
+	   SUM(CAST(new_vaccinations AS INT)) OVER (PARTITION BY de.location ORDER BY de.date )  running_total_vaccinations,
+	   SUM(CAST(new_vaccinations AS INT)) OVER (PARTITION BY de.location ) total_vaccinations
+  FROM covid_deaths de
+  JOIN covid_vaccinations va
+    ON de.location = va.location AND de.date = va.date
+  WHERE de.location = 'Egypt' AND va.new_vaccinations IS NOT NULL 
 
---(lookign at total population vs vaccinations)
-SELECT cd.continent , cd.location , cd.date , cd.population , cv.new_vaccinations,
-       SUM(cv.new_vaccinations::INTEGER) 
-           OVER (PARTITION BY cd.location ORDER BY cd.location , cd.date) rolling_people_vaccinated 
-  FROM coviddeaths cd
-  JOIN covidvaccinations cv
-    ON cd.location = cv.location AND cd.date = cv.date 
- WHERE cd.continent IS NOT NULL
- ORDER BY 2 , 3 
+--Showing percentage of people vaccninated in egypt 
+CREATE VIEW egypt_percentage_people_vaccinated AS
+SELECT etpv.location ,etpv.date, new_vaccinations  , running_total_vaccinations , 
+       ROUND(running_total_vaccinations/covid_deaths.population *100,2) running_total_percentage_of_people_vaccinated_per_population
+  FROM egypt_total_population_vs_total_vaccinations etpv
+  JOIN covid_deaths
+    ON etpv.location = covid_deaths.location 
 
--- (using cte) 
-WITH cte (continent,loaction , date , population ,new_vaccinations, rolling_people_vaccinated) AS 
+
+--Creating Temp Table-- 
+DROP TABLE  IF EXISTS #percentpopulationvaccinated
+
+CREATE TABLE #percentpopulationvaccinated
 (
-SELECT cd.continent , cd.location , cd.date , cd.population , cv.new_vaccinations,
-       SUM(cv.new_vaccinations::INTEGER) 
-           OVER (PARTITION BY cd.location ORDER BY cd.location , cd.date) rolling_people_vaccinated 
-  FROM coviddeaths cd
-  JOIN covidvaccinations cv
-    ON cd.location = cv.location AND cd.date = cv.date 
- WHERE cd.continent IS NOT NULL
- ORDER BY 2 , 3 
-)
-
-SELECT *, (rolling_people_vaccinated/population)*100 people_vaccinated
-  FROM cte
-  
---(create temp table) 
-DROP TABLE IF EXISTS percentpopulationvaccinated
-CREATE TEMP TABLE percentpopulationvaccinated
-(
-continent VARCHAR(255),
 location VARCHAR(255),
 date DATE,
-population NUMERIC,
+population NUMERIC , 
 new_vaccinations NUMERIC,
-rollingpeoplevaccinated NUMERIC
+running_total_vacinations NUMERIC,
+total_vaccinations NUMERIC
 )
 
-INSERT INTO percentpopulationvaccinated
-SELECT cd.continent , cd.location , cd.date , cd.population , cv.new_vaccinations::NUMERIC,
-       SUM(cv.new_vaccinations::INTEGER) 
-           OVER (PARTITION BY cd.location ORDER BY cd.location , cd.date) rolling_people_vaccinated 
-  FROM coviddeaths cd
-  JOIN covidvaccinations cv
-    ON cd.location = cv.location AND cd.date = cv.date 
- WHERE cd.continent IS NOT NULL
- ORDER BY 2 , 3 
- 
-SELECT *
-  FROM percentpopulationvaccinated
-  
---(create view to store data for later visualizations) 
-CREATE VIEW percentpopulationvaccinated AS
-SELECT SUM(new_cases::INTEGER) new_cases, SUM(new_deaths::INTEGER) new_deaths,
-       (SUM(new_deaths::REAL)/SUM(new_cases::REAL))*100 death_percentage
-  FROM coviddeaths 
- WHERE continent IS NOT NULL 
- ORDER BY 1,2,3 NULLS LAST
+INSERT INTO #percentpopulationvaccinated
+SELECT de.location , de.date , de.population , CAST(va.new_vaccinations AS INT) new_vaccinations ,
+	   SUM(CAST(new_vaccinations AS INT)) OVER (PARTITION BY de.location ORDER BY de.date )  running_total_vaccinations,
+	   SUM(CAST(new_vaccinations AS INT)) OVER (PARTITION BY de.location ) total_vaccinations
+  FROM covid_deaths de
+  JOIN covid_vaccinations va
+    ON de.location = va.location AND de.date = va.date
+  WHERE de.location = 'Egypt' AND va.new_vaccinations IS NOT NULL 
 
--- queries used for tableau visualization
-
---1 
-
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths,
-       SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-  From coviddeaths
---Where location like '%states%'
- where continent is not null 
---Group By date
- order by 1,2
-
-
-
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
-
-
---Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
---From PortfolioProject..CovidDeaths
-----Where location like '%states%'
---where location = 'World'
-----Group By date
---order by 1,2
-
-
--- 2. 
-
--- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
-
-Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-  From coviddeaths
---Where location like '%states%'
- Where continent is null 
-       and location not in ('World', 'European Union', 'International')
- Group by location
- order by TotalDeathCount desc
-
-
--- 3.
-
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-  From coviddeaths
---Where location like '%states%'
- Group by Location, Population
- order by PercentPopulationInfected desc
-
-
--- 4.
-
-
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  
-       Max((total_cases/population))*100 as PercentPopulationInfected
-  From coviddeaths
---Where location like '%states%'
- Group by Location, Population, date
- order by PercentPopulationInfected desc
+--CREATE CTE
+WITH cte (location,date,population,new_vaccinations,running_total_vaccinations,total_vaccinations)
+AS
+(
+SELECT de.location , de.date , de.population , CAST(va.new_vaccinations AS INT) new_vaccinations ,
+	   SUM(CAST(new_vaccinations AS INT)) OVER (PARTITION BY de.location ORDER BY de.date )  running_total_vaccinations,
+	   SUM(CAST(new_vaccinations AS INT)) OVER (PARTITION BY de.location ) total_vaccinations
+  FROM covid_deaths de
+  JOIN covid_vaccinations va
+    ON de.location = va.location AND de.date = va.date
+  WHERE de.location = 'Egypt' AND va.new_vaccinations IS NOT NULL 
+)
+--Showing percentage of people vaccninated in egypt 
+SELECT cte.location ,cte.date, new_vaccinations  , running_total_vaccinations , 
+       ROUND(running_total_vaccinations/covid_deaths.population *100,2) running_total_percentage_of_people_vaccinated_per_population
+  FROM cte 
+  JOIN covid_deaths
+    ON cte.location = covid_deaths.location 
